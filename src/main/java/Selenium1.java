5f1f65191cb88cb33fa0dbfe54ef743f9875a956
SATTDN23.01.02 Cao Thi Can <SATTDN23.01.02@logigear.com>
2023-03-01 08:56:15 +0700

    selenium 1

--- src/main/java/Selenium1.java
+++ src/main/java/Selenium1.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Selenium1 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Selenium1 {
+    public static ChromeDriver driver;
+    static void quit()
+    {
+        driver.quit();
+    }
+    static void func_login_TC1()
+    {
+        driver.findElement(By.xpath("//input[@type='text']"));
+        driver.findElement(By.xpath("//input[@type='password']"));
+    }
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\SATTDN23.01.02\\IdeaProjects\\Selenium1.1\\src\\main\\Drivers\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.navigate().to("http://www.railwayb2.somee.com/Account/Login.cshtml");
+        quit();
+        func_login_TC1();
+    }
+
+
 }
+
